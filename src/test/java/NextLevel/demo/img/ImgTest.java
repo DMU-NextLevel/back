a933f0c8cac36749c8b07209bab3f6d9fec98212
kwonhee1 <lkh8033@gmail.com>
2025-09-28 11:32:29 +0900

    add img test gradle

--- src/test/java/NextLevel/demo/img/ImgTest.java
+++ src/test/java/NextLevel/demo/img/ImgTest.java
@@ -1,20 +1,41 @@
 package NextLevel.demo.img;
 
 import NextLevel.demo.img.entity.ImgEntity;
+<<<<<<< Updated upstream
 import NextLevel.demo.img.service.ImgPath;
 import NextLevel.demo.img.service.ImgService;
 import NextLevel.demo.img.service.ImgTransaction;
 import org.assertj.core.api.Assertions;
+=======
+import NextLevel.demo.img.repository.ImgRepository;
+import NextLevel.demo.img.service.ImgPath;
+import NextLevel.demo.img.service.ImgService;
+import NextLevel.demo.img.service.ImgServiceImpl;
+import NextLevel.demo.img.service.ImgTransaction;
+import org.junit.jupiter.api.Assertions;
+>>>>>>> Stashed changes
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
+<<<<<<< Updated upstream
+=======
+import org.mockito.Spy;
+>>>>>>> Stashed changes
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+<<<<<<< Updated upstream
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.ActiveProfiles;
+=======
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoSpyBean;
+import org.springframework.transaction.annotation.Transactional;
+>>>>>>> Stashed changes
 import org.springframework.web.multipart.MultipartFile;
 
 import java.nio.file.Files;
@@ -26,8 +47,14 @@ import java.nio.file.Paths;
 @ExtendWith(MockitoExtension.class)
 public class ImgTest {
 
+<<<<<<< Updated upstream
     @Autowired
     private ImgService imgService;
+=======
+    private ImgService imgService;
+    @MockitoSpyBean
+    private ImgRepository imgRepository;
+>>>>>>> Stashed changes
 
     private MultipartFile mockImgFile;
     private String imgName = "imgName";
@@ -35,16 +62,43 @@ public class ImgTest {
 
     @BeforeEach
     public void setUp() {
+<<<<<<< Updated upstream
+=======
+        Mockito.lenient().when(imgRepository.getImgCount(Mockito.anyInt(), Mockito.anyString())).thenReturn(1L); // img count return 1;
+        imgService = new ImgServiceImpl(imgRepository);
+>>>>>>> Stashed changes
         // db 초기화
         Mockito.mockStatic(Files.class).when(()->Files.write(Mockito.any(), Mockito.any(byte[].class))).thenReturn(Paths.get("uri"));
         mockImgFile = new MockMultipartFile(imgName, imgName, "content type", imgData);
     }
 
+<<<<<<< Updated upstream
     @Test
     @ImgTransaction
     public void saveImgTest(ImgPath imgPath) {
         ImgEntity img = imgService.saveImg(mockImgFile, imgPath);
         Assertions.assertThat(img.getUri()).isEqualTo(imgName);
+=======
+    @Transactional
+    @ImgTransaction
+    public ImgEntity testImgFail(MultipartFile imgFile, RuntimeException e, ImgPath imgPath) {
+        ImgEntity img = imgService.saveImg(imgFile, imgPath);
+        if(e != null)
+            throw e;
+        return img;
+    }
+
+    @Test
+    public void saveImgTest() {
+        ImgEntity img = testImgFail(mockImgFile, null, null);
+        // db에 저장이 되었는지 + 파일이 존재하는지
+        ImgEntity dbImg = imgRepository.findById(img.getId()).get();
+
+        Assertions.assertAll(
+                ()->Assertions.assertEquals(dbImg.getId(), img.getId(), "dbImg is not equal input img"),
+                ()->Assertions.assertEquals(dbImg.getUri(), img.getUri(), "dbImg not equal input img")
+        );
+>>>>>>> Stashed changes
     }
 
 }
