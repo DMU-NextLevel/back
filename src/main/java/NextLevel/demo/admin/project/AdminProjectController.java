92ebbfa6f8d13d22dde23465ac1663b6a09e765d
kwonhee1 <lkh8033@gmail.com>
2025-11-04 17:52:48 +0900

    fix admin project controller
    add 유져별 project list 조회 api

--- src/main/java/NextLevel/demo/admin/project/AdminProjectController.java
+++ src/main/java/NextLevel/demo/admin/project/AdminProjectController.java
@@ -3,14 +3,22 @@ package NextLevel.demo.admin.project;
 import NextLevel.demo.common.SuccessResponse;
 import NextLevel.demo.project.ProjectStatus;
 import NextLevel.demo.project.project.service.ProjectDeleteService;
+import NextLevel.demo.project.project.service.ProjectService;
+import NextLevel.demo.user.dto.user.request.RequestMyPageProjectListDto;
+import NextLevel.demo.user.service.MypageProjectSelectService;
+import NextLevel.demo.user.service.UserService;
+import NextLevel.demo.user.service.UserValidateService;
 import NextLevel.demo.util.jwt.JWTUtil;
 import com.fasterxml.jackson.annotation.JsonCreator;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -21,9 +29,13 @@ public class AdminProjectController {
 
     private final ProjectDeleteService projectDeleteService;
     private final AdminProjectService adminProjectService;
+    private final MypageProjectSelectService mypageProjectListService;
+    private final UserValidateService userValidateService;
 
-    public ResponseEntity getProjectList() {
-        return null;
+    @PostMapping
+    public ResponseEntity<?> mypageProjectListSupporter(@RequestBody @Valid RequestMyPageProjectListDto dto) {
+        userValidateService.findUserWithUserId(dto.getUserId());
+        return ResponseEntity.status(HttpStatus.OK).body(new SuccessResponse("success", mypageProjectListService.mypageProjectList(dto)));
     }
 
     @PostMapping("/status/{projectId}")
